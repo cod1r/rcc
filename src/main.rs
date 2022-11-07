977cbd2c04a6a629a52edbf972fe841f62ff126b
cod1r <jasonho353@gmail.com>
2022-11-07 16:45:54 -0600

    can read files

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+use std::fs::File;
+use std::io::Read;
+use std::env;
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    if env::args().len() < 2 {
+        panic!("number of args low");
+    }
+    let mut file = File::open(args[1].trim()).expect("expected file to open");
+    let mut contents = String::new();
+    file.read_to_string(&mut contents)
+        .expect("expected read_to_string to work");
+    print!("{}", contents);
 }
