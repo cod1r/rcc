8d921dd4a3759a6d359f015f1d2912aab99ca57d
cod1r <jasonho353@gmail.com>
2023-06-05 05:59:42 -0500

    Planning out how to organize C declarations and meanings

--- src/parser.rs
+++ src/parser.rs
@@ -203,5 +203,168 @@ impl Expr {
         }
     }
 }
+pub enum StorageClassSpecifier {
+    TypeDef,
+    Extern,
+    Static,
+    ThreadLocal,
+    Auto,
+    Register,
+}
+pub struct TypeName {}
+pub enum StructOrUnion {
+    Struct,
+    Union,
+}
+pub struct Declarator {}
+pub enum StructDeclarator {
+    Declarator(Declarator),
+    DeclaratorBitField {
+        declarator: Option<Declarator>,
+        const_expr: Expr,
+    },
+}
+pub struct SpecifierQualifierList {
+    pub type_qualifiers: Vec<TypeQualifier>,
+    pub type_specifiers: Vec<TypeSpecifier>,
+    pub alignment_specifiers: Vec<AlignmentSpecifier>,
+}
+pub struct StructDeclaration {
+    specifier_qualifier_list: SpecifierQualifierList,
+    struct_declarator_list: Vec<StructDeclarator>,
+}
+pub struct StructUnionSpecifier {
+    pub struct_or_union: StructOrUnion,
+    pub identifier: Option<usize>,
+    pub struct_declaration_list: Vec<StructDeclaration>,
+}
+pub struct EnumSpecifier {}
+pub enum TypeSpecifier {
+    Void,
+    Char,
+    Short,
+    Int,
+    Long,
+    Float,
+    Double,
+    Signed,
+    Unsigned,
+    _Bool,
+    _Complex,
+    _Atomic(TypeName),
+    StructUnion(StructUnionSpecifier),
+    Enum(EnumSpecifier),
+}
+pub enum TypeQualifier {
+    Const,
+    Restrict,
+    Volatile,
+    _Atomic,
+}
+pub enum FunctionSpecifier {
+    Inline,
+    _Noreturn,
+}
+pub enum AlignmentSpecifier {
+    _Alignas(TypeName),
+    _AlignasConstExpr(Expr),
+}
+impl Declarator {
+    fn new() -> Declarator {
+        Declarator {}
+    }
+}
+pub struct Declaration {
+    pub storage_class_specifiers: Vec<StorageClassSpecifier>,
+    pub type_specifiers: Vec<TypeSpecifier>,
+    pub type_qualifiers: Vec<TypeQualifier>,
+    pub function_specifiers: Vec<FunctionSpecifier>,
+    pub alignment_specifiers: Vec<AlignmentSpecifier>,
+    pub declarator: Option<Declarator>,
+}
+impl Declaration {
+    fn new() -> Declaration {
+        Declaration {
+            storage_class_specifiers: Vec::new(),
+            type_specifiers: Vec::new(),
+            type_qualifiers: Vec::new(),
+            function_specifiers: Vec::new(),
+            alignment_specifiers: Vec::new(),
+            declarator: None,
+        }
+    }
+}
+fn parse_declarations(
+    tokens: &[lexer::Token],
+    start_index: usize,
+) -> Result<Option<(Declaration, usize)>, String> {
+    let mut declaration_index = start_index;
+    let mut declaration = Declaration::new();
+    while declaration_index < tokens.len() {
+        match tokens[declaration_index] {
+            lexer::Token::KEYWORD_TYPEDEF => declaration
+                .storage_class_specifiers
+                .push(StorageClassSpecifier::TypeDef),
+            lexer::Token::KEYWORD_EXTERN => declaration
+                .storage_class_specifiers
+                .push(StorageClassSpecifier::Extern),
+            lexer::Token::KEYWORD_STATIC => declaration
+                .storage_class_specifiers
+                .push(StorageClassSpecifier::Static),
+            lexer::Token::KEYWORD__THREAD_LOCAL => declaration
+                .storage_class_specifiers
+                .push(StorageClassSpecifier::ThreadLocal),
+            lexer::Token::KEYWORD_AUTO => declaration
+                .storage_class_specifiers
+                .push(StorageClassSpecifier::Auto),
+            lexer::Token::KEYWORD_REGISTER => declaration
+                .storage_class_specifiers
+                .push(StorageClassSpecifier::Register),
+            lexer::Token::KEYWORD_VOID => declaration.type_specifiers.push(TypeSpecifier::Void),
+            lexer::Token::KEYWORD_CHAR => declaration.type_specifiers.push(TypeSpecifier::Char),
+            lexer::Token::KEYWORD_SHORT => declaration.type_specifiers.push(TypeSpecifier::Short),
+            lexer::Token::KEYWORD_INT => declaration.type_specifiers.push(TypeSpecifier::Int),
+            lexer::Token::KEYWORD_LONG => declaration.type_specifiers.push(TypeSpecifier::Long),
+            lexer::Token::KEYWORD_FLOAT => declaration.type_specifiers.push(TypeSpecifier::Float),
+            lexer::Token::KEYWORD_DOUBLE => declaration.type_specifiers.push(TypeSpecifier::Double),
+            lexer::Token::KEYWORD_SIGNED => declaration.type_specifiers.push(TypeSpecifier::Signed),
+            lexer::Token::KEYWORD_UNSIGNED => {
+                declaration.type_specifiers.push(TypeSpecifier::Unsigned)
+            }
+            lexer::Token::KEYWORD__BOOL => declaration.type_specifiers.push(TypeSpecifier::_Bool),
+            lexer::Token::KEYWORD__COMPLEX => {
+                declaration.type_specifiers.push(TypeSpecifier::_Complex)
+            }
+            lexer::Token::KEYWORD_STRUCT | lexer::Token::KEYWORD_UNION => todo!(),
+            lexer::Token::KEYWORD_ENUM => todo!(),
+            lexer::Token::KEYWORD_CONST => declaration.type_qualifiers.push(TypeQualifier::Const),
+            lexer::Token::KEYWORD_RESTRICT => {
+                declaration.type_qualifiers.push(TypeQualifier::Restrict)
+            }
+            lexer::Token::KEYWORD_VOLATILE => {
+                declaration.type_qualifiers.push(TypeQualifier::Volatile)
+            }
+            lexer::Token::KEYWORD__ATOMIC => {
+                todo!()
+            }
+            lexer::Token::KEYWORD_INLINE => declaration
+                .function_specifiers
+                .push(FunctionSpecifier::Inline),
+            lexer::Token::KEYWORD__NORETURN => declaration
+                .function_specifiers
+                .push(FunctionSpecifier::_Noreturn),
+            lexer::Token::KEYWORD__ALIGNAS => todo!(),
+            _ => todo!(),
+        }
+        todo!()
+    }
+    todo!()
+}
 
-pub fn parser(tokens: &[lexer::Token]) {}
+pub fn parser(tokens: &[lexer::Token]) -> Result<(), String> {
+    let mut parser_index = 0;
+    while parser_index < tokens.len() {
+        parser_index += 1;
+    }
+    todo!()
+}
